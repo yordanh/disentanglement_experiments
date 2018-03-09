0070feda760594805befd22e84f60d599b83263b
Yordan Hristov <yshristov@gmail.com>
2018-03-09 14:05:49 +0000

    add data_generator as an initialisation parameter for models

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     #Load Data
     data_generator = DataGenerator(folder_name="data/etg/",
                                    number_of_images=150,
+                                   number_of_objects=12,
                                    image_size=100,
                                    data_split=0.8, 
                                    number_to_augment=number_to_augment)
@@ -18,7 +19,7 @@ if __name__ == "__main__":
     cherry_picked_data = data_generator.cherry_pick(folder_name="cherry_picked")
 
     # Vanilla Convolutional Autoencoder
-    # vanilla_conv_ae = VanillaConvAE(number_to_augment=number_to_augment)
+    # vanilla_conv_ae = VanillaConvAE(data_generator=data_generator)
     # vanilla_conv_ae.build()
     # vanilla_conv_ae.compile(optimizer="adadelta", loss="binary_crossentropy")
     # vanilla_conv_ae.fit(x=x_train, 
@@ -36,12 +37,12 @@ if __name__ == "__main__":
     # vanilla_conv_ae.inspect_latent(encoded_imgs)
 
     # Beta Variational Convolutional Autoencoder
-    beta_conv_vae = BetaConvVAE(beta=20, number_to_augment=number_to_augment)
+    beta_conv_vae = BetaConvVAE(beta=20, data_generator=data_generator)
     beta_conv_vae.build()
     beta_conv_vae.compile(optimizer="adadelta")
     beta_conv_vae.fit(x=x_train, 
                 y=None,
-                epochs=1000,
+                epochs=1,
                 batch_size=128,
                 shuffle=True,
                 validation_data=(x_valid, None))
@@ -51,11 +52,19 @@ if __name__ == "__main__":
 
     # print(encoded_imgs)
     beta_conv_vae.plot_learning_curve()
-    beta_conv_vae.reconstruct(encoded_imgs, decoded_imgs, x_test)
-    beta_conv_vae.inspect_latent(encoded_imgs)
-    beta_conv_vae.generate()
+    beta_conv_vae.plot_real_vs_reconstruct(decoded_imgs, x_test)
+    beta_conv_vae.plot_latent_vectors(encoded_imgs)
+    beta_conv_vae.plot_sampled_images()
 
-    beta_conv_vae.inspect_latent_distribution(cherry_picked_data)
+    beta_conv_vae.plot_latent_distribution(cherry_picked_data)
+
+    # Save Model
+    beta_conv_vae.save_model()
+
+    beta_conv_vae_1 = BetaConvVAE(beta=20, data_generator=data_generator)
+    beta_conv_vae_1.load_model()
+
+    beta_conv_vae_1.plot_latent_distribution(cherry_picked_data)
 
     # Variational Convolutional Autoencoder with Auxiliary Cost Functions
     
